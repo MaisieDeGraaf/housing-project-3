ac2a5d327e3b8fc4ebf8e5050133fba05079cfe5
RSkrep <robert.p.skrepnek@gmail.com>
2024-02-26 20:17:03 -0500

    Update roberts.visuals.js

--- roberts.visuals.js
+++ roberts.visuals.js
@@ -10,7 +10,7 @@ canvas.height = 400;
 document.body.appendChild(canvas);
 
 // Step 3: Define data and options for your bar graph
-let data = {
+let data1 = {
   labels: ['label1', 'label2', 'label3', 'label4', 'label5', 'label6'],
   datasets: [{
     label: 'sale',
@@ -34,7 +34,76 @@ let data = {
     borderWidth: 1
   }]
 };
-
+let data2 = {
+    labels: ['label1', 'label2', 'label3', 'label4', 'label5', 'label6'],
+    datasets: [{
+      label: 'sale',
+      data: [],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };let data3 = {
+    labels: ['label1', 'label2', 'label3', 'label4', 'label5', 'label6'],
+    datasets: [{
+      label: 'sale',
+      data: [],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };let data4 = {
+    labels: ['label1', 'label2', 'label3', 'label4', 'label5', 'label6'],
+    datasets: [{
+      label: 'sale',
+      data: [],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };
 let options = {
   scales: {
     yAxes: [{
@@ -47,8 +116,22 @@ let options = {
 
 // Step 4: Create a new Chart instance with the canvas element
 let ctx = canvas.getContext('2d');
-let myChart = new Chart(ctx, {
+let myChart1 = new Chart(ctx, {
   type: 'bar',
-  data: data,
+  data: data1,
   options: options
 });
+let myChart2 = new Chart(ctx, {
+    type: '',
+    data: data2,
+    options: options
+  });let myChart3 = new Chart(ctx, {
+    type: '',
+    data: data3,
+    options: options
+  });let myChart4 = new Chart(ctx, {
+    type: '',
+    data: data4,
+    options: options
+  });
+  

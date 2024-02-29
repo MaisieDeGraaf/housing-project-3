a3db98c3ae9d94c2f72c6426be07e67012d5147e
Pooja14n <pooja.14.n@gmail.com>
2024-02-28 22:00:10 -0500

    updated

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,165 +1,314 @@
-// function createMap(housing) {
 
-//   // Create the tile layer that will be the background of our map.
-//   let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//   });
+// API endpoint"
+let queryUrl = "static/data/housingdata.geojson";
 
-
-//   // Create a baseMaps object to hold the streetmap layer.
-//   let baseMaps = {
-//     "Street Map": streetmap
-//   };
-
-//   // Create an overlayMaps object to hold the bikeStations layer.
-//   let overlayMaps = {
-//     "Properties": housing
-//   };
-
-//   // Create the map object with options.
-//   let map = L.map("map-id", {
-//     center: [40.73, -74.0059],
-//     zoom: 12,
-//     layers: [streetmap, housing]
-//   });
-
-//   // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-//   L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(map);
-// }
-
-// // function createMarkers(response) {
-
-// //   // Pull the "stations" property from response.data.
-// //   let stations = response.data.stations;
-
-// //   // Initialize an array to hold bike markers.
-// //   let bikeMarkers = [];
-
-// //   // Loop through the stations array.
-// //   for (let index = 0; index < stations.length; index++) {
-// //     let station = stations[index];
-
-// //     // For each station, create a marker, and bind a popup with the station's name.
-// //     let bikeMarker = L.marker([station.lat, station.lon])
-// //       .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
-
-// //     // Add the marker to the bikeMarkers array.
-// //     bikeMarkers.push(bikeMarker);
-// //   }
-
-// //   // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
-// //   createMap(L.layerGroup(bikeMarkers));
-// // }
+// let queryUrl = "http://127.0.0.1:5000/api/housing"
 
 
-// // Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
-// d3.json("housingdata.json");
-
-
-
-// Store our API endpoint as queryUrl.
-let query = "housingdata.geojson";
-
-// let myMap = L.map("map", {
-//   center: [43.62, -79.59],
-//   zoom: 9
-// });
-
-
-d3.json(query).then(function (data) {
-    // console.log(data)
-
-    createFeatures(data.features)
-
-});
-
-function createFeatures(housingData) {
-
-    function onOurFeature(feature, layer)  {       
-        layer.bindPopup(`<h3>Location: ${feature.properties.Address}</h3><hr><p>Listed Price: ${feature.properties.Price_Listed}</p>`);
-    }
-  
-    
-    function createMarker(feature, latlng){
-        let markers = {
-        //  radius:feature.properties.Price_Listed*2,
-         fillColor: chooseColor(feature.geometry.coordinates),
-         color: "black",
-         weight: 0.5,
-         opacity: 0.8,
-         fillOpacity: 0.7
-        } 
-        return L.circleMarker(latlng, markers);
-     }
-     
-     let houses = L.geoJSON(housingData, {
-         onEachFeature: onOurFeature,
-         pointToLayer: createMarker
-     });
- 
-     
-     createMap(houses);
- }
- 
- function chooseColor(listedprice){
-    if (listedprice < 100000) return "lime";
-    else if (listedprice < 300000) return "#D4E157";
-    else if (listedprice < 500000) return "#FFCA28";
-    else if (listedprice < 700000) return "#FF9800";
-    else if (listedprice < 900000) return "#FF7043";
-    else return "red";
+// function to add commas to List Price popup
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
 
+d3.json(queryUrl).then(data => {
 
-let legend = L.control({position: 'bottomright'});
+    createMap();
 
-legend.onAdd = function() {
-    let div = L.DomUtil.create('div', 'info legend');
-    let grades = [300000, 500000, 700000, 800000, 900000, 1000000];
-    let labels = [];
-    let legendInfo = "<h4></h4>";
-    
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + chooseColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
+    function createMap() {
 
-    return div;
-  };
-           
+        let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        });
 
-  function createMap(houses) {
-    //BASE MAPS
-  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  });
+        let baseMaps = {
+            "Street Map": streetmap
+            // "Light Map": lightmap
+        };
 
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
+        
+        var homeIcon = L.icon({
+            iconUrl: '../images/home.PNG',
 
-  let baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
-  };
+            iconSize: [15, 15], 
+            iconAnchor: [7, 7], 
+            popupAnchor: [0, -10] 
+        });
 
-  let overlayMaps = {
-    "Houses" : houses
-  }
+        var greenIcon = L.icon({
+            iconUrl: '..images/green.jpg',
 
-  let myMap = L.map("map", {
-    center: [43.62, -79.59],
-    zoom: 9,
-    layers: [street, houses]
-  });
+            iconSize: [15, 15], 
+            iconAnchor: [7, 7], 
+            popupAnchor: [0, -10] 
+        });
 
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-  legend.addTo(myMap);
-    
-  }
+        var blueIcon = L.icon({
+            iconUrl: '../images/blue.png',
 
+            iconSize: [15, 15], 
+            iconAnchor: [7, 7], 
+            popupAnchor: [0, -10] 
+        });
+
+        var orangeIcon = L.icon({
+            iconUrl: '../images/orange.png',
+
+            iconSize: [15, 15], 
+            iconAnchor: [7, 7], 
+            popupAnchor: [0, -10] 
+        });
+
+        var yellowIcon = L.icon({
+            iconUrl: '../images/yellow.jpg',
+
+            iconSize: [15, 15], 
+            iconAnchor: [7, 7], 
+            popupAnchor: [0, -10] 
+        });
+
+        var redIcon = L.icon({
+            iconUrl: '../images/red.png',
+
+            iconSize: [15, 15], 
+            iconAnchor: [7, 7], 
+            popupAnchor: [0, -10] 
+        });
+
+
+        const layers = {
+            active: [],
+            one: new L.LayerGroup(),
+            two: new L.LayerGroup(),
+            three: new L.LayerGroup(),
+            four: new L.LayerGroup(),
+            five: new L.LayerGroup(),
+            six: new L.LayerGroup()
+        };
+
+
+        layers.one = L.geoJson(data, {
+            filter: function (feature, layer) {
+                return (feature.properties.Price_Listed <= 250000);
+            },
+
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, { icon: greenIcon });
+            },
+
+            onEachFeature: function (feature, layer) {
+                
+                layer.bindPopup(
+                    '<h3>' +
+                    feature.properties.Address +
+                    '</h3><hr><p>' +
+                    '$' +
+                    numberWithCommas(feature.properties.Price_Listed) + ' / ' +
+                    feature.properties.Bedrooms + ' bedrooms' + ' / ' +
+                    feature.properties.Bathrooms + ' baths' + ' / ' +
+                    feature.properties.Status + ' status' + ' / ' +
+                    '</p>')
+            }
+        })
+
+        layers.two = L.geoJson(data, {
+            filter: function (feature, layer) {
+                return (
+                    feature.properties.Price_Listed >= 250000 &&
+                    feature.properties.Price_Listed <= 500000
+                )
+            },
+
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, { icon: blueIcon });
+            },
+
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(
+                    '<h3>' +
+                    feature.properties.Address +
+                    '</h3><hr><p>' +
+                    '$' +
+                    numberWithCommas(feature.properties.Price_Listed) + ' / ' +
+                    feature.properties.Bedrooms + ' bedrooms' + ' / ' +
+                    feature.properties.Bathrooms + ' baths' + ' / ' +
+                    feature.properties.Status + ' status' + ' / ' +
+                    '</p>')
+            }
+        })
+
+        layers.three = L.geoJson(data, {
+            filter: function (feature, layer) {
+                return (
+                    feature.properties.Price_Listed >= 500000 &&
+                    feature.properties.Price_Listed <= 750000
+                )
+            },
+
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, { icon: orangeIcon });
+            },
+
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(
+                    '<h3>' +
+                    feature.properties.Address +
+                    '</h3><hr><p>' +
+                    '$' +
+                    numberWithCommas(feature.properties.Price_Listed) + ' / ' +
+                    feature.properties.Bedrooms + ' bedrooms' + ' / ' +
+                    feature.properties.Bathrooms + ' baths' + ' / ' +
+                    feature.properties.Status + ' status' + ' / ' +
+                    '</p>')
+            }
+        })
+
+        layers.four = L.geoJson(data, {
+            filter: function (feature, layer) {
+                return (
+                    feature.properties.Price_Listed >= 750000 &&
+                    feature.properties.Price_Listed <= 1000000
+                )
+            },
+
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, { icon: yellowIcon });
+            },
+
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(
+                    '<h3>' +
+                    feature.properties.Address +
+                    '</h3><hr><p>' +
+                    '$' +
+                    numberWithCommas(feature.properties.Price_Listed) + ' / ' +
+                    feature.properties.Bedrooms + ' bedrooms' + ' / ' +
+                    feature.properties.Bathrooms + ' baths' + ' / ' +
+                    feature.properties.Status + ' status' + ' / ' +
+                    '</p>')
+            }
+        })
+
+        layers.five = L.geoJson(data, {
+            filter: function (feature, layer) {
+                return (feature.properties.Price_Listed >= 1000000);
+            },
+
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, { icon: redIcon });
+            },
+
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(
+                    '<h3>' +
+                    feature.properties.Address +
+                    '</h3><hr><p>' +
+                    '$' +
+                    numberWithCommas(feature.properties.Price_Listed) + ' / ' +
+                    feature.properties.Bedrooms + ' bedrooms' + ' / ' +
+                    feature.properties.Bathrooms + ' baths' + ' / ' +
+                    feature.properties.Status + ' status' + ' / ' +
+                    '</p>')
+            }
+        })
+       
+ 
+        d3.json(queryUrl).then(citydata => {
+            
+            cityMap();
+            maplegend();
+
+            function cityMap() {
+
+                let circleStyle = {};
+                circleStyle.Oakville = {
+                    color: "green",
+                    fillColor: "lightgreen",
+                    fillOpacity: 0.2,
+                    radius: 250,
+                    weight: 0
+                };
+                circleStyle.Burlington = {
+                    color: "blue",
+                    fillColor: "lightblue",
+                    fillOpacity: 0.3,
+                    radius: 250,
+                    weight: 0
+                };
+                circleStyle.Vaughan = {
+                    color: "yellow",
+                    fillColor: "yellow",
+                    fillOpacity: 0.2,
+                    radius: 250,
+                    weight: 0
+                };
+                circleStyle.Milton = {
+                    color: "pink",
+                    fillColor: "pink",
+                    fillOpacity: 0.2,
+                    radius: 250,
+                    weight: 0
+                };
+                circleStyle.Oshawa = {
+                    color: "red",
+                    fillColor: "red",
+                    fillOpacity: 0.2,
+                    radius: 250,
+                    weight: 0
+                };
+
+
+                // School Quality layer - mimics a heatmap based on school rating
+                layers.six = L.geoJson(citydata, {
+
+                    // call "features" of geojson file
+                    filter: function (feature, layer) {
+                        return citydata.features;
+                    },
+
+                    pointToLayer: function (feature, latlng) {
+                        let city = feature.properties.City;
+                        return L.circle(latlng, circleStyle[city]); 
+                    },
+
+                })
+            }
+        });
+
+
+        function maplegend() {
+            
+            let overlayMaps = {
+                "<img src='../images/green.jpg' width = 15 /> <span>Up to $250K</span>": layers.one,
+                "<img src='../images/blue.png' width = 15 /> <span>$250K - $500K</span>": layers.two,
+                "<img src='../images/orange.png' width = 15 /> <span>$500K - $750K</span>": layers.three,
+                "<img src='../images/yellow.jpg' width = 15 /> <span>$750K - $1M</span>": layers.four,
+                "<img src='../images/red.png' width = 15 /> <span>$1M+</span>": layers.five,
+                "City": layers.six
+            };
+
+            let myMap = L.map("map", {
+                center: [
+                    43.62, -79.59
+                ],
+                zoom: 9.5,
+                layers: [
+                    streetmap
+                    // layers.one,
+                    // layers.two,
+                    // layers.three,
+                    // layers.four,
+                    // layers.five
+                ]
+            });
+
+            
+            L.control.layers(baseMaps, overlayMaps, {
+                collapsed: false
+            }).addTo(myMap);
+
+            L.control.scale(position = 'topleft').addTo(myMap);
+
+            
+        } 
+    };
+});

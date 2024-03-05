a29bfb74de114cc4c26152eed0997bb2c3eabe27
Qwertykitten <146257477+MaisieDeGraaf@users.noreply.github.com>
2024-03-05 16:28:41 -0500

    Update visuals.js

--- static/js/visuals.js
+++ static/js/visuals.js
@@ -4,6 +4,7 @@ let weatherURL = "http://127.0.0.1:5000/api/v1.0/weather";
 
 d3.json(weatherURL).then(weatherData => {
     let currentDate = new Date();
+<<<<<<< Updated upstream
     let currentMonth = currentDate.getMonth() + 1;
     let currentDay = currentDate.getDate();
     
@@ -16,16 +17,30 @@ d3.json(weatherURL).then(weatherData => {
     let averageMinTemperature = weatherDataForCurrentMonthDay.reduce((acc, curr) => acc + curr.min_temperature, 0) / weatherDataForCurrentMonthDay.length;
     let averageWindSpeed = weatherDataForCurrentMonthDay.reduce((acc, curr) => acc + curr.wind_speed, 0) / weatherDataForCurrentMonthDay.length;
     let averagePrecipitation = weatherDataForCurrentMonthDay.reduce((acc, curr) => acc + curr.total_precipitation, 0) / weatherDataForCurrentMonthDay.length;
+=======
+    let currentDateFormatted = currentDate.toISOString().split('T')[0];
+
+    let weatherDataForCurrentDate = weatherData.filter(entry => {
+        let entryDateFormatted = new Date(entry.local_date).toISOString().split('T')[0];
+        return entryDateFormatted === currentDateFormatted;
+    });
+    console.log("Weather data for current month and day:", weatherDataForCurrentDate);
+
+    let averageMaxTemperature = weatherDataForCurrentDate.reduce((acc, curr) => acc + curr.max_temperature, 0) / weatherDataForCurrentDate.length;
+    let averageMinTemperature = weatherDataForCurrentDate.reduce((acc, curr) => acc + curr.min_temperature, 0) / weatherDataForCurrentDate.length;
+    let averageWindSpeed = weatherDataForCurrentDate.reduce((acc, curr) => acc + curr.wind_speed, 0) / weatherDataForCurrentDate.length;
+    let averagePrecipitation = weatherDataForCurrentDate.reduce((acc, curr) => acc + curr.total_precipitation, 0) / weatherDataForCurrentDate.length;
+>>>>>>> Stashed changes
 
     let weatherBox = document.getElementById("weather-box");
     weatherBox.innerHTML = `
-        <h2>Weather Information for ${currentMonth}/${currentDay}</h2>
+        <h2>Weather Information for ${currentDateFormatted}</h2>
         <p>Average Max Temperature: ${averageMaxTemperature.toFixed(2)}</p>
         <p>Average Min Temperature: ${averageMinTemperature.toFixed(2)}</p>
         <p>Average Wind Speed: ${averageWindSpeed.toFixed(2)}</p>
         <p>Average Precipitation: ${averagePrecipitation.toFixed(2)}</p>
     `;
-})
+});
 
 d3.json(URL)
   .then(function(data) {
